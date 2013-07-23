79db6f2c6c48f84d8e29b015bf51bccc8e96d93d
Claudio Ortolina <claudio@new-bamboo.co.uk>
2013-07-23 19:31:23 +0100

    Much shorter anagram logic.

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -8,19 +8,9 @@ defmodule Anagram do
   """
 
   def match(word, list) do
-    do_match(word, list, [])
-  end
-
-  defp do_match(_word, [], acc), do: Enum.reverse(acc)
-  defp do_match(word, [head|tail], acc) do
-    cond do
-      compare_words(word, head) -> do_match(word, tail, [head|acc])
-      true -> do_match(word, tail, acc)
+    Enum.filter list, fn(potential_anagram) ->
+      sort_word(word) == sort_word(potential_anagram)
     end
-  end
-
-  defp compare_words(word1, word2) do
-    sort_word(word1) == sort_word(word2)
   end
 
   defp sort_word(word) do
