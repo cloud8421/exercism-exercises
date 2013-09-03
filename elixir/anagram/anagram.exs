700d18164281494e0f5f5cb0fbe9570f46688518
Claudio Ortolina <claudio@new-bamboo.co.uk>
2013-09-03 21:47:12 +0100

    Updates elixir anagram.

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -9,8 +9,17 @@ defmodule Anagram do
 
   def match(word, list) do
     Enum.filter list, fn(potential_anagram) ->
-      sort_word(word) == sort_word(potential_anagram)
+      match_word(word, potential_anagram)
     end
+  end
+
+  def match_word(word, potential_anagram) do
+    do_match_word(String.downcase(word), String.downcase(potential_anagram))
+  end
+
+  defp do_match_word(word, word), do: false
+  defp do_match_word(word, potential_anagram) do
+    sort_word(word) == sort_word(potential_anagram)
   end
 
   defp sort_word(word) do
--- elixir/anagram/anagram_test.exs
+++ elixir/anagram/anagram_test.exs
@@ -12,13 +12,18 @@ defmodule AnagramTest do
   end
 
   test "detect simple anagram" do
-    matches = Anagram.match "ba", ["ab", "abc", "bac"]
-    assert matches == ["ab"]
+    matches = Anagram.match "ant", ["tan", "stand", "at"]
+    assert matches == ["tan"]
   end
 
   test "detect multiple anagrams" do
-    matches = Anagram.match "abc", ["ab", "abc", "bac"]
-    assert matches == ["abc", "bac"]
+    matches = Anagram.match "master", ["stream", "pigeon", "maters"]
+    assert matches == ["stream", "maters"]
+  end
+
+  test "do not detect anagram subsets" do
+    matches = Anagram.match "good", %w(dog goody)
+    assert matches == []
   end
 
   test "detect anagram" do
@@ -29,5 +34,25 @@ defmodule AnagramTest do
   test "multiple anagrams" do
     matches = Anagram.match "allergy", %w(gallery ballerina regally clergy largely leading)
     assert matches == ["gallery", "regally", "largely"]
+  end
+
+  test "detect anagrams case-insensitively" do
+    matches = Anagram.match "Orchestra", %w(cashregister Carthorse radishes)
+    assert matches == ["Carthorse"]
+  end
+
+  test "anagrams must not be the source word" do
+    matches = Anagram.match "banana", ["banana"]
+    assert matches == []
+  end
+
+  test "anagrams must not be the source word case-insensitively" do
+    matches = Anagram.match "banana", ["Banana"]
+    assert matches == []
+  end
+
+  test "anagrams must use all letters exactly once" do
+    matches = Anagram.match "patter", ["tapper"]
+    assert matches == []
   end
 end
